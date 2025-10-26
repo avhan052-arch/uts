d5e2f9841c7c7f5703eba2daf5808f625a884c1d
avhan052-arch <avhan052@gmail.com>
2025-10-27 06:42:55 +0800

    perbaikan error motion initialize

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:motion/motion.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
  await Motion.instance.initialize();
   runApp(const MaterialApp(
   home: MainApp(),
